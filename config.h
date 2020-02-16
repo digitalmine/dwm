174a0d118c2322468ded0280469487197e4df00b
digitalmine <34872176+digitalmine@users.noreply.github.com>
2020-02-16 18:17:34 +0000

    change color of focused window border

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
+static char selbordercolor[]        = "#ffff00";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
